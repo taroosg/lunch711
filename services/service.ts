ff8f1eae179539d816d785d37777515812ca5f72
taroosg <root@taroosg.dev>
2022-05-31 18:02:51 +0900

    update global product

--- services/service.ts
+++ services/service.ts
@@ -3,19 +3,22 @@ import "https://deno.land/x/dotenv/load.ts";
 
 const getAllProductData = async (url: string) => {
   const response = await fetch(url);
-  const result = (await response.text())
-    .replaceAll(/\n|\r|\t/g, "")
-    .match(/<div class="flex_wrap">[\s\S]+<!-- flex_wrap -->/g) ?? [];
-  return result[0]
-    .split("<!-- list_inner -->")
-    .filter((x, i, arr) => x !== " " && i !== 0 && i !== arr.length - 1)
+
+  return (await response.text())
+    .replaceAll(/\n|\r|\t/g, "")!
+    .match(/<div class="list_inner(?: .+?)?>.*?<\/div><\/div>/g)!
     .map((x) => {
       return {
+        // 記号数字を全角→半角
+        // 全角スペース→半角スペースに
+        // 連続スペースを一つに
         name: x
-          .match(/<p><a href=[\s\S]+<\/a><\/p>/)![0]
           .match(
-            /[^ -~｡-ﾟ]+/g,
+            /(?<=<p><a href="\/products\/a\/item\/[0-9]{4,}\/">)[\s\S]+(?=<\/a><\/p>)/,
           )![0]
+          // .match(
+          //   /[^ -~｡-ﾟ]+/g,
+          // )![0]
           .replaceAll(/　/g, " ")
           .replace(
             /[！-～]/g,
@@ -23,11 +26,15 @@ const getAllProductData = async (url: string) => {
           )
           .replace(/(?<=\S) {2,}/g, " "),
         price: ~~Number(x
-          .match(/(?<=item_price"><p>)[\s\S]+）<\/p><\/div>/g)![0]
           .match(/(?<=（税込)[\s\S]+(?=円）)/)),
         place: x
-          .match(/<span>販売地域：<\/span>[\s\S]+<\/p><\/div>/g)![0]
-          .match(/(?<=<\/span>)[\s\S]+(?=<\/p><\/div>)/)![0].split("、"),
+          .match(/(?<=<span>販売地域：<\/span>)[\s\S]+(?=<\/p><\/div>)/)![0]
+          .split("、"),
+        url: `https://www.sej.co.jp/${
+          x.match(
+            /(?<=<p><a href=")[\s\S]+(?=\/\">)/,
+          )![0]
+        }`,
         img: x.match(/https:[\s\S]+.jpg/)![0],
       };
     });
@@ -46,10 +53,8 @@ const getRegion = (prefecture: string) =>
   placeData.region.find((x) => x.pref.find((x) => x.ja === prefecture))?.ja;
 
 export const getRegionalData = async (latitude: number, longitude: number) => {
-  const lat = latitude ?? 35.68381981;
-  const lng = longitude ?? 139.77456498;
   // 都道府県
-  const place = await getPlace(lat, lng);
+  const place = await getPlace(latitude, longitude);
   // 地方
   const region = getRegion(place);
   // 全部
@@ -60,8 +65,12 @@ export const getRegionalData = async (latitude: number, longitude: number) => {
   const placeProducts = allProducts.filter((x) => x.place.includes(place));
   // 地方
   const regionProducts = allProducts.filter((x) => x.place.includes(region!));
+  // 全国
+  const globalProducts = allProducts.filter((x) => x.place[0] === "全国");
   // 実行地域で購入できるやつ
-  const canGetProducts = [...new Set([...placeProducts, ...regionProducts])];
+  const canGetProducts = [
+    ...new Set([...placeProducts, ...regionProducts, ...globalProducts]),
+  ];
   const rand = ~~(Math.random() * canGetProducts.length);
   return canGetProducts[rand];
 };
